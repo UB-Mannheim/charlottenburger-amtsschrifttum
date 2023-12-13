86ef4b7341a28c2a70894dbd35523a87322ea653
Stefan Weil <sw@weilnetz.de>
2023-12-13 14:43:19 +0100

    Download required PNG images instead of TIFF
    Signed-off-by: Stefan Weil <sw@weilnetz.de>

--- data/download_images.sh
+++ data/download_images.sh
@@ -3,4 +3,4 @@
 cd $(dirname $0)
 
 urlbase=`echo -n "aHR0cHM6Ly9kaWdpdGFsLnpsYi5kZS92aWV3ZXIvYXBpL3YxL3JlY29yZHMvCg==" | base64 -d`
-cat imageurls.list | while read -r line; do wget -O "${line#* }" "${urlbase}${line% *}/files/images/${line#* }/full/max/0/default.tif"; done
+cat imageurls.list | while read -r line; do image=${line#* }; ext=$(echo $image|grep -o "...$");  wget -O "$image" "${urlbase}${line% *}/files/images/${image/png/tif}/full/max/0/default.$ext"; done
